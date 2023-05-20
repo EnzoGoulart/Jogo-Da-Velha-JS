769cd2836f1bcd2e5fbdb7c148a3779c1cf18af3
EnzoGoulart <enzogithub123@gmail.com>
2023-05-20 16:52:51 -0300

    Correcao de bugs

--- ttt.js
+++ ttt.js
@@ -1,20 +1,12 @@
 let currentPlayer = 'X';
 let VerificaChange = false
-let c0 = document.getElementById('c0');
-let c1 = document.getElementById('c1');
-let c2 = document.getElementById('c2');
-let c3 = document.getElementById('c3');
-let c4 = document.getElementById('c4');
-let c5 = document.getElementById('c5');
-let c6 = document.getElementById('c6');
-let c7 = document.getElementById('c7');
-let c8 = document.getElementById('c8');
+let botao = document.getElementById('botao');
 let reiniciar = document.getElementById('botao2')
 reiniciar.style.display = 'none'
 let infos = document.getElementById('divInfos')
 infos.style.display = 'none'
 let time = document.getElementById('times');
-resultado = document.getElementById('result')
+let resultado = document.getElementById('result')
 resultado.style.display = 'none'
 let totPartidas = 0
 let xWins = 0
@@ -22,32 +14,41 @@ let oWins = 0
 let totDraws = 0
 let gameEnded = false;
 const cells = document.querySelectorAll('.cell');
-
 cells.forEach(cell => {
   cell.addEventListener('click', clicado);
 });
 function clicado(event) {
   botao.innerHTML = 'Proxima'
   const cell = event.target;
-  console.log(cell.innerHTML)
   if (cell.textContent === "" && !gameEnded) {
     cell.innerHTML = `<p class="inDiv">${currentPlayer}</p>`;
     checkGameStatus();
     togglePlayer();
     recomecar()
+    console.log('Cheguei 1')
   }
 }
 
 function togglePlayer() {
-  currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+  if(currentPlayer == 'X'){
+    currentPlayer = 'O'
+  }else{
+    currentPlayer = 'X'
+  }
   time.innerHTML = `Vez do ${currentPlayer}`;
+  console.log('Player mudou'+currentPlayer)
 }
 
 function checkGameStatus() {
   const winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
-    [0, 4, 8], [2, 4, 6] 
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], 
+    [0, 4, 8],
+    [2, 4, 6] 
   ];
 
   for (comb of winningCombinations) {
@@ -90,30 +91,34 @@ function announceDrawn() {
   resultado.style.display = 'block'
   resultado.innerHTML = 'Empate!'
 }
-let botao = document.getElementById('botao');
 async function recomecar() {
-  botao.addEventListener('click', () => {
-    ChangePlayer()
-    togglePlayer()
-    resultado.style.display = 'none'
-    time.style.display = 'block'
-    cells.forEach(cell => {
-      cell.innerHTML = ''
-    })
-    gameEnded = false
-    totPartidas == 0 ? infos.style.display = 'none' : infos.style.display = 'block'
-    infos.style.display == 'block' ? reiniciar.style.display = 'block' : reiniciar.style.display = 'none'
-    infos.innerHTML = `<p class="pInfo" id="pInfo1">Partida atual - ${totPartidas + 1}</p>`
-    infos.innerHTML += `<p class="pInfo">X:${xWins} vs O:${oWins}</p>`
-    totDraws != 1 ? infos.innerHTML += `<p class="pInfo">${totDraws} empates</p>` : infos.innerHTML += `<p class="pInfo" id="pInfo4">${totDraws} empate</p>`
-  });
-}
+  resultado.style.display = 'none'
+  time.style.display = 'block'
+  gameEnded = false
+  totPartidas == 0 ? infos.style.display = 'none' : infos.style.display = 'block'
+  infos.style.display == 'block' ? reiniciar.style.display = 'block' : reiniciar.style.display = 'none'
+  infos.innerHTML = `<p class="pInfo" id="pInfo1">Partida atual - ${totPartidas + 1}</p>`
+  infos.innerHTML += `<p class="pInfo">X:${xWins} vs O:${oWins}</p>`
+  totDraws != 1 ? infos.innerHTML += `<p class="pInfo">${totDraws} empates</p>` : infos.innerHTML += `<p class="pInfo" id="pInfo4">${totDraws} empate</p>`
+  };
+
 reiniciar.addEventListener('click', () => {
   location.reload()
 })
+
 function ChangePlayer(){
   VerificaChange = Array.from(cells).every((cell) => cell.textContent === '');
-  if(VerificaChange){
+  if(VerificaChange && totPartidas >0){
     botao.innerHTML = 'Mudar Jogador'
+    console.log('mudar jogador')
   }
 }
+
+botao.addEventListener('click',() => {
+  console.log('botao clicado')
+  cells.forEach(cell => {
+    cell.innerHTML = ''
+  })
+  ChangePlayer()
+  togglePlayer()
+  })
